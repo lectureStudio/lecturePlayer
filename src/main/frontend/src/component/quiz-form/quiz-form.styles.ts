ca62787ee5ba15cc65d401ee1c59bd592385444f
Alex Andres <andres.alex@protonmail.com>
2023-02-28 16:15:48 +0100

    Refactored quiz-form to use shoelace components

--- src/main/frontend/src/component/quiz-form/quiz-form.styles.ts
+++ src/main/frontend/src/component/quiz-form/quiz-form.styles.ts
@@ -4,4 +4,25 @@ export const quizFormStyles = css`
 	:host {
 		width: 100%;
 	}
+
+	form {
+		gap: 0;
+	}
+
+	ol, ul, dl {
+		margin: 1rem 0;
+	}
+
+	sl-radio-group::part(form-control-input) {
+		display: flex;
+		flex-direction: column;
+		gap: 0.75em;
+	}
+
+	.quiz-options {
+		display: flex;
+		flex-direction: column;
+		gap: 0.75em;
+		padding-bottom: 0.5rem;
+	}
 `;
--- src/main/frontend/src/component/quiz-form/quiz-form.ts
+++ src/main/frontend/src/component/quiz-form/quiz-form.ts
@@ -19,46 +19,40 @@ export class QuizForm extends I18nLitElement {
 
 	render() {
 		const itemTemplates = new Array<TemplateResult>();
+		const type = this.feature?.type;
 
-		this.feature?.options.forEach((option: string, index: number) => {
-			switch (this.feature.type) {
-				case QuizType.Multiple:
-					itemTemplates.push(html`
-						<div class="form-check">
-							<input class="form-check-input" type="checkbox" name="options" value="${index}" id="option-${index}">
-							<label class="form-check-label" for="option-${index}">${option}</label>
-						</div>
-					`);
-					break;
-
-				case QuizType.Single:
-					itemTemplates.push(html`
-						<div class="form-check">
-							<input class="form-check-input" type="radio" name="options" value="${index}" id="option-${index}">
-							<label class="form-check-label" for="option-${index}">${option}</label>
-						</div>
-					`);
-					break;
-
-				case QuizType.Numeric:
-					itemTemplates.push(html`
-						<div class="form-check">
-							<label for="option-${index}" class="form-label">${option}</label>
-							<input type="text" class="form-control form-control-sm" name="options" id="option-${index}">
-						</div>
-					`);
-					break;
-			}
-		});
+		if (type === QuizType.Multiple) {
+			this.feature?.options.forEach((option: string, index: number) => {
+				itemTemplates.push(html`
+					<sl-checkbox name="options" value="${index}" id="option-${index}">${option}</sl-checkbox>
+				`);
+			});
+		}
+		else if (type === QuizType.Single) {
+			itemTemplates.push(html`
+				<sl-radio-group name="options">
+					${this.feature?.options.map((option: string, index: number) =>
+						html`<sl-radio value="${index}" id="option-${index}">${option}</sl-radio>`
+					)}
+				</sl-radio-group>
+			`);
+		}
+		else if (type === QuizType.Numeric) {
+			this.feature?.options.forEach((option: string, index: number) => {
+				itemTemplates.push(html`
+					<sl-input label="${option}" name="options" id="option-${index}" size="small"></sl-input>
+				`);
+			});
+		}
 
 		return html`
 			<form id="quiz-form">
 				<input type="hidden" name="serviceId" value="${this.feature?.featureId}" />
 
-				<div class="mb-1">
+				<div class="quiz-question">
 					${unsafeHTML(this.feature?.question)}
 				</div>
-				<div class="py-2">
+				<div class="quiz-options">
 					${itemTemplates}
 				</div>
 			</form>
