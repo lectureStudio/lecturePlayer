72db25895354d898e1825ea0ef9371e2d436b8f8
Alex Andres <andres.alex@protonmail.com>
2023-08-20 22:34:23 +0200

    Fixed speech api

--- src/transport/course-speech-api.ts
+++ src/transport/course-speech-api.ts
@@ -3,11 +3,14 @@ import { HttpRequest } from "../utils/http-request";
 export namespace CourseSpeechApi {
 
 	export function requestSpeech(courseId: number) {
-		return new HttpRequest().post<string>(`/api/v1/course/speech/${courseId}`);
+		return new HttpRequest()
+			.setResponseType("text")
+			.post<string>(`/api/v1/course/speech/${courseId}`);
 	}
 
 	export function cancelSpeech(courseId: number, requestId: string) {
-		return new HttpRequest().post<void>(`/api/v1/course/speech/${courseId}/${requestId}`);
+		return new HttpRequest()
+			.delete(`/api/v1/course/speech/${courseId}/${requestId}`);
 	}
 
 }
