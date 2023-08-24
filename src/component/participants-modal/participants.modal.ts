28ef75fb2a8a1bb5a7f5b2061febef1388012e25
Alex Andres <andres.alex@protonmail.com>
2023-08-24 11:14:44 +0200

    Fixed participants-list height

--- src/component/participants-modal/participants.modal.ts
+++ src/component/participants-modal/participants.modal.ts
@@ -2,9 +2,16 @@ import { html } from "lit";
 import { Modal } from "../modal/modal";
 import { customElement } from "lit/decorators.js";
 import { t } from '../i18n-mixin';
+import participantsModalStyles from "./participants.modal.scss";
 
 @customElement("participants-modal")
 export class ParticipantsModal extends Modal {
+
+	static styles = [
+		Modal.styles,
+		participantsModalStyles
+	];
+
 
 	protected render() {
 		return html`
